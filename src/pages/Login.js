ed68fa828084e05bbf0c6651a384ae1066066ab9
CristianoNunes <cristianonunes99@gmail.com>
2019-08-08 15:42:25 -0300

    configurando rotas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
 export default function Login(){
+    const [username, setUsername] = useState('');
+
+    function handleSubmit(){
+        
+    }
+
     return(
         <div className="login-container">
             <form>
                 <img src={logo} alt="Tindev" />
-                <input placeholder="Digite seu usuário do GitHub" />
+                <input 
+                    placeholder="Digite seu usuário do GitHub" 
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
                 <button type="submit">Enviar</button>
             </form>
         </div>
